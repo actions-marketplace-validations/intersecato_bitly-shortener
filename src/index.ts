11d517989edd5a07eb50475341fad2e82dca05f9
Pedro <npedrini2003@icloud.com>
2023-09-02 17:23:53 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import * as core from '@actions/core'
+import * as https from 'https'
+import {BitlyLink} from './types'
+
+const run = (): void => {
+  const longUrl: string = core.getInput('long_url')
+  const bitlyToken: string = core.getInput('bitly_token')
+  const customDomain: string = core.getInput('bitly_custom_domain')
+  const linkTitle: string = core.getInput('link_title')
+
+  const data = JSON.stringify({
+    long_url: longUrl,
+    domain: customDomain || 'bit.ly',
+    title: linkTitle || undefined
+  })
+  
+  const options = {
+    hostname: 'api-ssl.bitly.com',
+    path: '/v4/bitlinks',
+    method: 'POST',
+    json: true,
+    headers: {
+      Authorization: `Bearer ${bitlyToken}`,
+      'Content-Type': 'application/json'
+    }
+  }
+
+  core.info(`Requesting Bit.ly short URL for: ${longUrl}`)
+  
+  const req = https.request(options, response => {
+    let body = ''
+    response.setEncoding('utf8');
+    response.on('data', (chunk) => {
+      body += chunk
+    });
+    response.on('end', () => {
+      const link = JSON.parse(body) as BitlyLink;
+      core.info(`Bit.ly short URL: ${link.link}`)
+      core.setOutput('bitly_link', link.link)
+    });
+  })
+
+  req.on('error', (e) => {
+    core.setFailed(e.message)
+  });
+
+  req.on('timeout', () => {
+    core.setFailed(`Request timed out`)
+  })
+
+  req.write(data)
+  req.end()
+}
+
+run()
